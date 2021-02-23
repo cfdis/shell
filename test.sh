1db4956e4b2995fb8f4b3fc25a12f4058e008f0d
Eliut <eislasq@gmail.com>
2021-02-23 02:28:31 -0600

    api

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
-api_id='9fLMd08+Sb0dGe618eomGJKXPA7KTtpr'
-api_secret='1r/LRKryEuIuUurPLP94qtdRzzZomZ8u'
+api_id='API_ID'
+api_secret='API_SECRET'
 token=$(echo -ne "$api_id:$api_secret" | base64 --wrap 0)
 
 curl "http://backend.demo.facturabilidad.com/api/Cfdi33/timbrar"\
